24a71b2538d425ce99d5fdca7dbdbaa728ce3c9b
AlejandroAntonPineda <alejandroantonpineda@gmail.com>
2023-06-19 14:20:06 +0200

    screen

---
+++ screen.py
@@ -0,0 +1,7 @@
+import pygame
+
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+background = pygame.Surface(screen.get_size())
+background.fill((255, 255, 255))
+screen.blit(background, (0, 0))
+screen_size = screen.get_size()
